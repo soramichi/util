ea0747196efff4d028d6d566f1dc43339fa979f7
Soramichi Akiyama <akiyama@m.soramichi.jp>
2017-02-12 11:05:58 +0900

    show detached correctly

--- display_branch.sh
+++ display_branch.sh
@@ -1,6 +1,6 @@
 ret=`git branch 2>/dev/null | grep "*"`
 
 if [ "$?" == "0" ]; then
-    branch=`echo "$ret" | cut -d ' ' -f 2`
+    branch=`echo "$ret" | cut -d ' ' -f 2-`
     echo " [$branch]"
 fi
